deefbc2a1324b613d4bf36cfa8ccbca7ed0c9554
Mariano Jimenez Brenes <mariano@Tesla.Home>
2019-12-30 10:29:55 -0600

    uart works fine

---
+++ ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/DependInfo.cmake
@@ -0,0 +1,78 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/startup/startup_stm32f407xx.s" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/startup/startup_stm32f407xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/STM32F4xx_HAL_Driver/Src"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_usart.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_usart.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/src/gpio.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/src/gpio.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/src/main.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/src/main.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/src/stm32f4xx_hal_msp.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/src/stm32f4xx_hal_msp.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/src/stm32f4xx_it.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/src/stm32f4xx_it.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/src/syscall.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/src/syscall.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/src/system_stm32f4xx.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/src/system_stm32f4xx.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/src/tim.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/src/tim.c.obj"
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/src/usart.c" "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/src/usart.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../Drivers/STM32F4xx_HAL_Driver/Src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/mariano/Triverlogic/Repositories/uart/ProjectToBuild/build/Drivers/STM32F4xx_HAL_Driver/Src/CMakeFiles/HAL_LIBRARY.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ProjectToBuild/build/CMakeFiles/basic_example.elf.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic_example.elf.dir/src/main.c.obj"
+  "CMakeFiles/basic_example.elf.dir/src/gpio.c.obj"
+  "CMakeFiles/basic_example.elf.dir/src/tim.c.obj"
+  "CMakeFiles/basic_example.elf.dir/src/usart.c.obj"
+  "CMakeFiles/basic_example.elf.dir/src/stm32f4xx_it.c.obj"
+  "CMakeFiles/basic_example.elf.dir/src/stm32f4xx_hal_msp.c.obj"
+  "CMakeFiles/basic_example.elf.dir/src/syscall.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_usart.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "CMakeFiles/basic_example.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "CMakeFiles/basic_example.elf.dir/src/system_stm32f4xx.c.obj"
+  "CMakeFiles/basic_example.elf.dir/startup/startup_stm32f407xx.s.obj"
+  "bin/basic_example.elf.pdb"
+  "bin/basic_example.elf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/basic_example.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
